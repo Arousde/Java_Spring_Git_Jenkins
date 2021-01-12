80a999b3502653fbbc2f07bca2c2c25e1e0834e2
Oussema <arous.oussema@gmail.com>
2021-01-12 20:21:28 +0100

    ok

--- src/com/arous/Main.java
+++ src/com/arous/Main.java
@@ -4,7 +4,6 @@ package com.arous;
 public class Main {
 
     public static void main(String[] args) {
-        String first = args[0];
         System.out.println("hello world");
         System.out.println("master branch");
     }

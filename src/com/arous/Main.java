36dfb6e50f66091ceb74356144ff6249baf6ea8c
Oussema <arous.oussema@gmail.com>
2021-01-12 13:21:18 +0100

    f3 ok

--- src/com/arous/Main.java
+++ src/com/arous/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("f3");
+        System.out.println("ok3");
     }
 }
